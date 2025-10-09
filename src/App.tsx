7b9c35c62fa69077c944275662626d03ed7d688e
Christian <christiandave120702@gmail.com>
2025-10-09 10:09:22 +0800

    feat: Create main App component to structure layout and integrate existing components

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import SearchForm from './components/SearchForm'
+import Features from './components/Features'
+import Notice from './components/Notice'
+import Footer from './components/Footer'
+import { Outlet, useLocation } from 'react-router-dom'
+
+function App() {
+  const location = useLocation();
+  const isProductsPage = location.pathname === '/products';
+
+  return (
+    <div className="flex flex-col min-h-screen text-slate-900 bg-white">
+      <Navbar />
+      {!isProductsPage && (
+        <main className="px-4">
+          <Hero />
+          <SearchForm />
+          <Features />
+          <Notice />
+        </main>
+      )}
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
