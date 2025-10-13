05c582f5a5a6975a72be68c6b13d7a8607a2b7bf
Christian <christiandave120702@gmail.com>
2025-10-13 20:28:34 +0800

    feat: Integrate ThemeProvider and update routing with placeholders for analytics and report pages

--- src/main.tsx
+++ src/main.tsx
@@ -7,26 +7,30 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import Products from './pages/Products.tsx'
 import BatchVerify from './pages/BatchVerify.tsx'
 import About from './pages/About.tsx'
+import { ThemeProvider } from 'next-themes'
 
-const queryClinet = new QueryClient()
+const queryClient = new QueryClient()
 
 const routes = createBrowserRouter([
   {
-    path: "/", element: <App />,
+    path: '/',
+    element: <App />,
     children: [
-      { path: "products", element: <Products /> },
-      { path: "batch-verify", element: <BatchVerify /> },
-      { path: "analytics", element: "" },
-      { path: "about", element: <About /> },
-      { path: "report", element: "" },
-    ]
+      { path: 'products', element: <Products /> },
+      { path: 'batch-verify', element: <BatchVerify /> },
+      { path: 'analytics', element: <div className='text-center'>Coming soon</div> },
+      { path: 'about', element: <About /> },
+      { path: 'report', element: <div className='text-center'>Coming soon</div> },
+    ],
   },
 ])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClinet}>
-      <RouterProvider router={routes} />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="data-theme" defaultTheme="light" enableSystem={false} storageKey="theme" disableTransitionOnChange>
+        <RouterProvider router={routes} />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
