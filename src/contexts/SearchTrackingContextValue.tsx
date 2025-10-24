d5d3c63a8ba7c88920aff10de516182026785a83
Christian <christiandave120702@gmail.com>
2025-10-24 16:01:51 +0800

    remove

--- src/contexts/SearchTrackingContextValue.tsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react';
-import type { SearchTrackingContextType } from '../types/searchTracking';
-
-export const SearchTrackingContext = createContext<SearchTrackingContextType | undefined>(undefined);
